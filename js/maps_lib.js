d154580424b4403e3dc8f359ef734437a48f059b
ctfredri <ctfredri@users.noreply.github.com>
2015-06-18 22:34:24 -0700

    Update maps_lib.js

--- js/maps_lib.js
+++ js/maps_lib.js
@@ -77,6 +77,8 @@ var MapsLib = {
       document.getElementById('search_address'));
     //map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
+    var searchBox = new google.maps.places.SearchBox(
+    /** @type {HTMLInputElement} */(input));  
     
     // maintains map centerpoint for responsive design
     google.maps.event.addDomListener(map, 'idle', function() {
